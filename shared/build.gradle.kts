49fa88c2ca3f87327f95184f7349de098e1de762
unchil <yeopo77@icloud.com>
2024-12-26 08:00:03 +0900

    Initial commit

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,5 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -22,23 +20,6 @@ kotlin {
     iosSimulatorArm64()
     
     jvm()
-
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-    }
 
 
     sourceSets {
--- shared/src/wasmJsMain/kotlin/org/example/ktor/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package org.example.ktor
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
