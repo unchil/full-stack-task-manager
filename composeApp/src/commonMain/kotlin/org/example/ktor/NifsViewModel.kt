3a9a6004f035e0285a198aed46f55496a509a550
unchil <yeopo77@icloud.com>
2025-03-19 10:47:30 +0900

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/example/ktor/NifsViewModel.kt
@@ -0,0 +1,68 @@
+package org.example.ktor
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.example.ktor.data.NifsRepository
+import org.example.ktor.model.Observation
+import org.example.ktor.model.Observatory
+
+class NifsViewModel ( private val scope: CoroutineScope) {
+
+    private val repository: NifsRepository
+        = getPlatform().nifsRepository
+
+    val _observationOneDayStateFlow: MutableStateFlow<List<Observation>>
+            = MutableStateFlow(emptyList())
+
+    val _observationCurrentStateFlow: MutableStateFlow<List<Observation>>
+            = MutableStateFlow(emptyList())
+
+    val _observatoryStateFlow: MutableStateFlow<List<Observatory>>
+            = MutableStateFlow(emptyList())
+
+
+    init {
+        scope.launch {
+            repository.getObservatory()
+            repository.getObservations("current")
+
+            repository._observatoryStateFlow.collectLatest {
+                _observatoryStateFlow.value = it
+            }
+            repository._observationCurrentStateFlow.collectLatest {
+                _observationCurrentStateFlow.value = it
+            }
+            repository._observationOneDayStateFlow.collectLatest {
+                _observationOneDayStateFlow.value = it
+            }
+        }
+    }
+
+    suspend fun onEvent(event: Event) {
+        when (event) {
+            is Event.ObservationRefresh -> {
+                getObservations(event.division)
+            }
+            Event.ObservatoryRefresh -> {
+                getObservatory()
+            }
+        }
+
+    }
+
+    suspend fun getObservations(division: String){
+        repository.getObservations(division)
+    }
+
+    suspend fun getObservatory(){
+        repository.getObservatory()
+    }
+
+    sealed class Event {
+        data class ObservationRefresh(val division: String) : Event()
+        data object ObservatoryRefresh : Event()
+    }
+
+}
