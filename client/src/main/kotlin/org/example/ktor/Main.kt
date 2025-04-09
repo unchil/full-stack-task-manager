e4682d5db0e2eb8aa3829eb7b3aba012133e3f7c
unchil <yeopo77@icloud.com>
2025-04-10 07:51:39 +0900

    Initial commit

--- client/src/main/kotlin/org/example/ktor/Main.kt
+++ client/src/main/kotlin/org/example/ktor/Main.kt
@@ -1,14 +1,6 @@
 package org.example.ktor
 
 
-import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.api.*
-import org.jetbrains.kotlinx.dataframe.io.*
-import kotlin.io.path.Path
-
-
-
-
 suspend fun main() {
 
     val repository = Repository()
