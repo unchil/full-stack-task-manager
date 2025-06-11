3bb520049ed52e116152e2732c9a0dafeab27b5e
unchil <yeopo77@icloud.com>
2025-06-11 20:06:29 +0900

    Initial commit

--- client/src/main/kotlin/org/example/ktor/Main.kt
+++ client/src/main/kotlin/org/example/ktor/Main.kt
@@ -1,12 +1,8 @@
 package org.example.ktor
 
-import ch.qos.logback.core.util.Loader.getResource
-import com.apple.eio.FileManager.getResource
 import io.ktor.util.logging.*
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.datetime.format.FormatStringsInDatetimeFormats
 import org.jetbrains.kotlinx.dataframe.DataRow
-import org.jetbrains.kotlinx.dataframe.io.readJson
 
 val LOGGER = KtorSimpleLogger( "client")
 
