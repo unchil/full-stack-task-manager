6efce251aef03142f9e188dd3a76956d9073feb3
unchil <yeopo77@icloud.com>
2025-08-27 16:37:35 +0900

    Initial commit

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -14,25 +14,26 @@ application {
     mainClass.set("org.example.ktor.MainKt")
 }
 
-dependencies {
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio)
-    implementation(libs.logback)
-    implementation(libs.ktor.client.logging)
-    implementation(libs.ktor.client.negotiation)
-    implementation(libs.kotlinx.serialization)
-    implementation(libs.ktor.serialization.json)
-    implementation(libs.ktor.serialization.xml)
-    implementation(libs.kotlinx.dataframe)
-    implementation(libs.sqlite)
-    implementation(libs.exposed.core)
-    implementation(libs.exposed.jdbc)
+kotlin {
 
-    implementation("org.json:json:20250517")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_21) // 또는 "17", "23" 등
+    }
+
+    dependencies {
+        implementation(libs.ktor.client.core)
+        implementation(libs.ktor.client.cio)
+        implementation(libs.logback)
+        implementation(libs.ktor.client.logging)
+        implementation(libs.ktor.client.negotiation)
+        implementation(libs.kotlinx.serialization)
+        implementation(libs.ktor.serialization.json)
+        implementation(libs.ktor.serialization.xml)
+        implementation(libs.kotlinx.dataframe)
+        implementation(libs.sqlite)
+        implementation(libs.exposed.core)
+        implementation(libs.exposed.jdbc)
+
+        implementation("org.json:json:20250517")
     }
 }
