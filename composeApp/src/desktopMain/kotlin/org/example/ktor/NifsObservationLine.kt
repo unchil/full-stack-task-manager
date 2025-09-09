4cd2347131193e2b4c9275bb566cb7a487d2d20e
unchil <yeopo77@icloud.com>
2025-09-09 13:06:26 +0900

    Initial commit

--- composeApp/src/desktopMain/kotlin/org/example/ktor/NifsObservationLine.kt
+++ composeApp/src/desktopMain/kotlin/org/example/ktor/NifsObservationLine.kt
@@ -1,33 +1,37 @@
 package org.example.ktor
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.format
-import kotlinx.datetime.format.byUnicodePattern
 import org.example.ktor.data.DATA_DIVISION
 import org.jetbrains.letsPlot.geom.geomLine
-import org.jetbrains.letsPlot.ggsize
 import org.jetbrains.letsPlot.intern.Plot
-import org.jetbrains.letsPlot.label.labs
 import org.jetbrains.letsPlot.letsPlot
-import org.jetbrains.letsPlot.scale.scaleYContinuous
 import org.jetbrains.letsPlot.skia.compose.PlotPanel
-import org.jetbrains.letsPlot.themes.elementText
-import org.jetbrains.letsPlot.themes.theme
 
 @Composable
 fun NifsObservationLine(modifier: Modifier = Modifier) {
     MaterialTheme {
 
         val viewModel = remember { NifsSeaWaterInfoOneDayViewModel() }
+
+        var selectedOption by remember { mutableStateOf(SEA_AREA.GRU_NAME.entries[0]) }
 
         LaunchedEffect(key1 = viewModel){
             viewModel.onEvent(NifsSeaWaterInfoOneDayViewModel.Event.ObservationRefresh(DATA_DIVISION.oneday))
@@ -42,11 +46,36 @@ fun NifsObservationLine(modifier: Modifier = Modifier) {
         val preserveAspectRatio = remember { mutableStateOf(false) }
         val seaWaterInfoOneday = viewModel._seaWaterInfoOneDayStateFlow.collectAsState()
 
-        LaunchedEffect(key1= seaWaterInfoOneday.value){
-            figureLine = createLineChart(seaWaterInfoOneday.value.toLineData(SEA_AREA.GRU_NAME.EAST)   )
+        LaunchedEffect(key1= seaWaterInfoOneday.value, key2=selectedOption){
+            figureLine = createLineChart(selectedOption, seaWaterInfoOneday.value.toLineData(selectedOption)   )
         }
 
-        Row(modifier = Modifier.then(modifier).padding(vertical = 8.dp)) {
+        Column (modifier = Modifier.then(modifier).padding(vertical = 8.dp)) {
+
+            Row {
+                SEA_AREA.GRU_NAME.entries.forEach { entrie ->
+                    Row(
+                        Modifier
+                            .selectable(
+                                selected = (entrie == selectedOption),
+                                onClick = { selectedOption = entrie }
+                            )
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (entrie == selectedOption),
+                            onClick = { selectedOption = entrie }
+                        )
+                        Text(
+                            text = entrie.name,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                    }
+                }
+            }
+
+
             PlotPanel(
                 modifier = Modifier.size(width = 1300.dp, height = 400.dp),
                 figure = figureLine,
--- composeApp/src/desktopMain/kotlin/org/example/ktor/NifsObservationRibbon.kt
+++ composeApp/src/desktopMain/kotlin/org/example/ktor/NifsObservationRibbon.kt
@@ -1,11 +1,16 @@
 package org.example.ktor
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
@@ -26,6 +31,8 @@ fun NifsObservationRibbon(modifier: Modifier = Modifier) {
 
         val viewModel = remember { NifsSeaWaterInfoStatViewModel() }
 
+        var selectedOption by remember { mutableStateOf(SEA_AREA.GRU_NAME.entries[0]) }
+
         LaunchedEffect(key1 = viewModel){
             viewModel.onEvent(NifsSeaWaterInfoStatViewModel.Event.ObservationStatRefresh)
             while(true){
@@ -39,11 +46,36 @@ fun NifsObservationRibbon(modifier: Modifier = Modifier) {
         val preserveAspectRatio = remember { mutableStateOf(false) }
         val seaWaterInfoStat = viewModel._seaWaterInfoStatStateFlow.collectAsState()
 
-        LaunchedEffect(key1= seaWaterInfoStat.value){
-            figureLine = createRibbonChart(seaWaterInfoStat.value.toRibbonData(SEA_AREA.GRU_NAME.EAST))
+        LaunchedEffect(key1= seaWaterInfoStat.value, key2=selectedOption){
+            figureLine = createRibbonChart(selectedOption, seaWaterInfoStat.value.toRibbonData(selectedOption))
         }
 
-        Row(modifier = Modifier.then(modifier).padding(vertical = 8.dp)) {
+        Column (modifier = Modifier.then(modifier).padding(vertical = 8.dp)) {
+
+            Row {
+                SEA_AREA.GRU_NAME.entries.forEach { entrie ->
+                    Row(
+                        Modifier
+                            .selectable(
+                                selected = (entrie == selectedOption),
+                                onClick = { selectedOption = entrie }
+                            )
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (entrie == selectedOption),
+                            onClick = { selectedOption = entrie }
+                        )
+                        Text(
+                            text = entrie.name,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                    }
+                }
+            }
+
+
             PlotPanel(
                 modifier = Modifier.size(width = 1300.dp, height = 400.dp),
                 figure = figureLine,
@@ -52,6 +84,9 @@ fun NifsObservationRibbon(modifier: Modifier = Modifier) {
                 computationMessages.forEach { println("[APP MESSAGE] $it") }
             }
         }
+
+
+
 
     }
 }
