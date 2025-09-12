d19e226116ba4042326576275247b2f63b8881f9
unchil <yeopo77@icloud.com>
2025-09-12 17:22:38 +0900

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/example/ktor/Model.kt
@@ -0,0 +1,23 @@
+package org.example.ktor
+
+object SEA_AREA {
+
+    enum class GRU_NAME {
+        WEST, EAST, SOUTH
+    }
+
+    fun GRU_NAME.gru_nam():String {
+        return when(this) {
+            GRU_NAME.WEST -> "서해"
+            GRU_NAME.EAST -> "동해"
+            GRU_NAME.SOUTH -> "남해"
+        }
+    }
+
+    val gruNames = listOf(
+        GRU_NAME.WEST.gru_nam(),
+        GRU_NAME.SOUTH.gru_nam(),
+        GRU_NAME.EAST.gru_nam()
+    )
+
+}
