61ad606056f74e989e0f08ec0fc4e0963f09ab3c
unchil <yeopo77@icloud.com>
2025-09-18 17:00:18 +0900

    Initial commit

--- server/src/main/kotlin/org/example/ktor/module/NifsSerialization.kt
+++ server/src/main/kotlin/org/example/ktor/module/NifsSerialization.kt
@@ -49,12 +49,12 @@ fun Application.configureNifsSerialization(repository: NifsRepository) {
                 val now = System.currentTimeMillis()
 
                 // 1. 캐시에서 데이터 조회
-                val cachedData = cache_SeawaterInfo[key_SeawaterInfo]
-
-                if (cachedData != null && now - cachedData.second < TimeUnit.SECONDS.toMillis(CACHE_EXPIRY_SECONDS)) {
+                cache_SeawaterInfo[key_SeawaterInfo]?.let { it ->
+                    if( (now - it.second) < TimeUnit.SECONDS.toMillis(CACHE_EXPIRY_SECONDS) ){
                 // 2. 캐시가 유효하면 즉시 응답
-                    println("Serving from cache for ID: $division")
-                    return@get call.respond(cachedData.first)
+                        println("Serving from cache for ID: $division")
+                        return@get call.respond(it.first)
+                    }
                 }
                 // 3. 캐시가 없거나 만료되면 DB에서 데이터 조회
                 try {
@@ -65,10 +65,8 @@ fun Application.configureNifsSerialization(repository: NifsRepository) {
                     }
                 // 4. 새로운 데이터를 캐시에 저장
                     cache_SeawaterInfo[key_SeawaterInfo] = Pair(result, now)
-
                 // 5. 응답
                     call.respond(result)
-
                 } catch (ex: IllegalArgumentException) {
                     call.respond(HttpStatusCode.BadRequest)
                 }
@@ -77,10 +75,11 @@ fun Application.configureNifsSerialization(repository: NifsRepository) {
             get ("/stat"){
                 val key_SeaWaterInfoStatistics = "cache_stat"
                 val now = System.currentTimeMillis()
-                val cachedData = cache_SeaWaterInfoStatistics[key_SeaWaterInfoStatistics]
-                if (cachedData != null && now - cachedData.second < TimeUnit.SECONDS.toMillis(CACHE_EXPIRY_SECONDS)) {
-                    println("Serving from cache for ID: stat")
-                    return@get call.respond(cachedData.first)
+                cache_SeaWaterInfoStatistics[key_SeaWaterInfoStatistics]?.let {
+                    if( (now - it.second) < TimeUnit.SECONDS.toMillis(CACHE_EXPIRY_SECONDS) ){
+                        println("Serving from cache for ID: stat")
+                        return@get call.respond(it.first)
+                    }
                 }
                 try {
                     val result = repository.seaWaterInfoStatistics()
